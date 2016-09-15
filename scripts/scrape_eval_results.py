dc0e81a180370e100eb4c6af49a599080ec16d2b
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-09-15 19:38:43 +0900

    Use pandas to cleanup evaluation code

--- scripts/scrape_eval_results.py
+++ scripts/scrape_eval_results.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+from __future__ import division
 
 import glob
 import os.path as osp
 import yaml
-import numpy as np
+
+import pandas
 
 
 this_dir = osp.dirname(osp.realpath(__file__))
 
-single_view_iu = []
-label_octomap_iu = []
-max_single_view_iu = []
-label_octomap_exceeds_max = []
-label_octomap_exceeds_mean = []
-for result_file in glob.glob(osp.join(this_dir, '../eval_result/*.yaml')):
-    label = int(osp.splitext(osp.basename(result_file))[0])
-    result = yaml.load(open(result_file))
-    iu_view0 = result['viewpoint_0']
-    iu_view1 = result['viewpoint_1']
-    iu_view2 = result['viewpoint_2']
-    three_view_iu = [iu_view0, iu_view1, iu_view2]
-    single_view_iu.append(iu_view0)
-    single_view_iu.append(iu_view1)
-    single_view_iu.append(iu_view2)
-    max_iu = max(three_view_iu)
-    max_single_view_iu.append(max_iu)
-    iu_octomap = result['label_octomap']
-    label_octomap_iu.append(iu_octomap)
-    label_octomap_exceeds_max.append(iu_octomap > max_iu)
-    label_octomap_exceeds_mean.append(iu_octomap > np.mean(three_view_iu))
-print('mean IU for single view: ', np.mean(single_view_iu))
-print('mean of max IUs of three single views: ', np.mean(max_single_view_iu))
-print('mean IU for LabelOctoMap: ', np.mean(label_octomap_iu))
-print('ratio of LabelOctoMap exceeds max IU of three single views: ', np.mean(label_octomap_exceeds_max))
-print('ratio of LabelOctoMap exceeds mean IU of three single views: ', np.mean(label_octomap_exceeds_mean))
+result_files = glob.glob(osp.join(this_dir, '../eval_result/*.yaml'))
+
+columns = ['label', 'iu_view_0', 'iu_view_1', 'iu_view_2', 'iu_label_octomap']
+data = []
+filename_to_label = lambda x: int(osp.splitext(osp.basename(x))[0])
+for result_file in sorted(result_files, key=filename_to_label):
+    iu_data = yaml.load(open(result_file))
+    label_value = filename_to_label(result_file)
+    row = [
+        label_value,
+        iu_data['viewpoint_0'],
+        iu_data['viewpoint_1'],
+        iu_data['viewpoint_2'],
+        iu_data['label_octomap'],
+    ]
+    data.append(row)
+df = pandas.DataFrame(data=data, columns=columns)
+
+df_single_view = df[['iu_view_0', 'iu_view_1', 'iu_view_2']]
+mean_of_max_iu_single_view = df_single_view.max(axis=1).mean()
+mean_iu_single_view = df_single_view.sum(axis=1).sum() / df_single_view.size
+
+mean_iu_label_octomap = df['iu_label_octomap'].mean()
+
+print('mean_iu_single_view:', mean_iu_single_view)
+print('mean_of_max_iu_single_view:', mean_of_max_iu_single_view)
+print('mean_iu_label_octomap:', mean_iu_label_octomap)
