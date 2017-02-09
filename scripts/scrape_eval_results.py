30fddd270d54abef7f32a24b235e452816a716d7
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-09 17:35:20 +0900

    update scrape_eval_results.py

--- scripts/scrape_eval_results.py
+++ scripts/scrape_eval_results.py
@@ -12,41 +12,31 @@ import pandas
 
 this_dir = osp.dirname(osp.realpath(__file__))
 
-result_files = glob.glob(osp.join(this_dir, '../eval_result/*.yaml'))
+res_files = glob.glob(osp.join(this_dir, '../eval_result/*.yaml'))
 
-columns = [
-    'label',
-    'iu_view_0',
-    'iu_view_1',
-    'iu_view_2',
-    'iu_label_octomap',
-    'image_file',
-]
 data = []
-filename_to_label = lambda x: int(osp.splitext(osp.basename(x))[0])
-for result_file in sorted(result_files, key=filename_to_label):
-    iu_data = yaml.load(open(result_file))
-    label_value = filename_to_label(result_file)
-    img_file = str(label_value) + '.jpg'
-    row = [
-        label_value,
-        iu_data['viewpoint_0'],
-        iu_data['viewpoint_1'],
-        iu_data['viewpoint_2'],
-        iu_data['label_octomap'],
-        img_file,
-    ]
-    data.append(row)
+for fn in res_files:
+    label_value = int(osp.splitext(osp.basename(fn))[0])
+    with open(fn, 'r') as f:
+        datum = yaml.load(f)
+    data.append({
+        'label_value': label_value,
+        'single_res_voxel_max': 100 * datum['sr_v_max'],
+        'single_res_voxel_mean': 100 * datum['sr_v_mean'],
+        'single_res_box_max': 100 * datum['sr_b_max'],
+        'single_res_box_mean': 100 * datum['sr_b_mean'],
+        'label_oct_voxel': 100 * datum['lo_v'],
+        'label_oct_box': 100 * datum['lo_b'],
+        'img': 'img_%d.jpg' % label_value,
+        'seg': 'seg_%d.jpg' % label_value,
+    })
+columns = ['label_value', 'single_res_box_max', 'single_res_box_mean',
+           'single_res_box_max', 'single_res_box_mean',
+           'label_oct_voxel', 'label_oct_box',
+           'img', 'seg']
 df = pandas.DataFrame(data=data, columns=columns)
+df = df.sort_values('label_value')
+df = df.set_index('label_value')
+df = df.round(decimals=2)
 
-df_single_view = df[['iu_view_0', 'iu_view_1', 'iu_view_2']]
-mean_of_max_iu_single_view = df_single_view.max(axis=1).mean()
-mean_iu_single_view = df_single_view.sum(axis=1).sum() / df_single_view.size
-
-mean_iu_label_octomap = df['iu_label_octomap'].mean()
-
-print('mean_iu_single_view:', mean_iu_single_view)
-print('mean_of_max_iu_single_view:', mean_of_max_iu_single_view)
-print('mean_iu_label_octomap:', mean_iu_label_octomap)
-
-print(df.to_latex(float_format=lambda x: '%.3f' % x))
+print(df.to_latex())
